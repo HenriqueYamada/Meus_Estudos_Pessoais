a0b43980baf9030040b010a9cdc0ee8827ad8b28
Henrique Yamada <henriqueyamada01@gmail.com>
2026-03-03 14:58:41 -0300

    changing script.js and learning hoisting

--- Alura/Modo Carreira - Front-End/Curso_JavaScript/script.js
+++ Alura/Modo Carreira - Front-End/Curso_JavaScript/script.js
@@ -1,1 +1,20 @@
-console.log('Olá, Mundo!')
+// Tipos de variáveis:
+// 1 - Variável Global: apenas redefine o valor da variável
+
+idade1 = 18 //- daria erro se estivesse sem o 1 pois estou mudando o valor de uma varável que não existe, pois será criada na linha 14
+console.log('1 - Idade: ', idade1)
+
+// 2 - Var: Muda todas os valores das variáveis de mesmo nome do seu código inteiro!
+
+var idade2 = 18
+console.log('2 - Idade: ', idade2)
+
+// 3 - Let: O mais recomendado, pois ele altera a variável que estiver no  mesmo bloco
+
+let idade3 = 18
+console.log('3 - Idade: ', idade3)
+
+// 4 - Const: O valor da contante nunca será mudada
+
+const idade4 = 18
+console.log('4 - idade: ', idade4)
