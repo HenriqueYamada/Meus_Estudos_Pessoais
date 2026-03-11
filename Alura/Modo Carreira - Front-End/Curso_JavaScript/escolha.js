13a477e8e708b22830d4d4caeb1a5d29f0b195b3
Henrique Yamada <henriqueyamada01@gmail.com>
2026-03-11 13:23:43 -0300

    Learning switch case

---
+++ Alura/Modo Carreira - Front-End/Curso_JavaScript/escolha.js
@@ -0,0 +1,30 @@
+const notaDoAluno = 7
+
+switch (notaDoAluno) {
+    case 10:
+    case 9:
+        console.log('Excelente')
+        break
+
+    case 8:
+    case 7:
+        console.log('Bom')
+        break
+    
+    case 6:
+    case 5:
+    case 4:
+        console.log('Médio')
+        break
+    
+    case 3:
+    case 2:
+    case 1:
+    case 0: 
+        console.log('Ruim')
+        break
+
+    default:
+        console.log('Informe a nota')
+        break
+}
