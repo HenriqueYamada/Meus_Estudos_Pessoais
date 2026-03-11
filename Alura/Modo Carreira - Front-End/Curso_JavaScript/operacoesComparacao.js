eb7a4bd8d10b4b9abad9fa3a9b99424879a68179
Henrique Yamada <henriqueyamada01@gmail.com>
2026-03-11 11:16:37 -0300

    Learning compare operations

---
+++ Alura/Modo Carreira - Front-End/Curso_JavaScript/operacoesComparacao.js
@@ -0,0 +1,34 @@
+const idadeVinny = 36
+const minhaIdade = "18"
+
+//Operador de Igualdade
+const igualdade = idadeVinny == minhaIdade
+console.log('São iguais?: ', igualdade)
+
+//Operador de Diferença
+const diferente = idadeVinny != minhaIdade
+console.log('Idade do Vinny é diferente da minha?: ', diferente)
+
+//Operador de Maior que
+const maior = idadeVinny > minhaIdade
+console.log('Idade do Vinny é maior que a minha?: ', maior)
+
+//Operador de Menor que 
+const menor = idadeVinny < minhaIdade
+console.log('Idade do Vinny é menor que a minha?: ', menor)
+
+//Operador de Maior ou Igual que
+const maiorIgual = idadeVinny >= minhaIdade
+console.log('Idade do Vinny é maior ou igual a minha?: ', maiorIgual)
+
+//Operador de Menor ou Igual que
+const menorIgual = idadeVinny <= minhaIdade
+console.log('Idade do Vinny é menor ou igual a minha?: ', menorIgual)
+
+//Operador Comparação Estrita Igual
+const comparacaoEstritaIgual = idadeVinny === minhaIdade
+console.log('A idade do Vinny e a minha não estritamente iguais?: ', comparacaoEstritaIgual)
+
+//Operador Comparação Estrita Diferente
+const comparacaoEstritaDiferente = idadeVinny !== minhaIdade
+console.log('A idade do Vinny e a minha são estritamente diferentes?: ', comparacaoEstritaDiferente)
