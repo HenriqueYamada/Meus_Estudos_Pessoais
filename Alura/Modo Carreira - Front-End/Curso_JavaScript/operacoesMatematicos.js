4bf8008eb920da6f3d04b5ab56f874491119dcd2
Henrique Yamada <henriqueyamada01@gmail.com>
2026-03-11 10:50:19 -0300

    Learning arithmetic operations

---
+++ Alura/Modo Carreira - Front-End/Curso_JavaScript/operacoesMatematicos.js
@@ -0,0 +1,47 @@
+const n1 = 4
+const n2 = 8
+
+let soma = n1 + n2
+
+console.log('Soma: ', soma)
+
+let subtracao = n1 - n2
+
+console.log('Subtração: ', subtracao)
+
+let multiplicacao = n1 * n2
+
+console.log('Multiplicação: ', multiplicacao)
+
+let divisao = n1 / n2
+
+console.log('Divisão: ', divisao)
+
+const contaComplexa = n1 + n1 * n2
+
+console.log('Conta complexa: ', contaComplexa)
+
+const resto = 13 % 2
+
+console.log('Resto da divisão: ', resto)
+
+const potencia = 2 ** 3
+
+console.log('Potência: ', potencia)
+
+let contador = 5
+
+//contador = contador + 1
+contador++
+
+console.log('Contador++: ', contador)
+
+contador-- 
+
+console.log('Contador--: ', contador)
+
+let n3 =  2
+
+n3 = n3++ * 2
+
+console.log(n3)
