91e7be78fa80b7b41c3c5cad4173faec256f883a
Henrique Yamada <henriqueyamada01@gmail.com>
2026-03-12 12:32:57 -0300

    learning repeating loop while and do while

---
+++ Alura/Modo Carreira - Front-End/Curso_JavaScript/lacoEnquanto.js
@@ -0,0 +1,32 @@
+let cont = 0
+/*while (cont <= 10) {
+    console.log('Número atual: ', cont)
+    cont++
+}*/
+
+let num = 1
+
+/*do {
+    console.log('Número atual: ', num)
+    num++
+} while (num <= 10)*/
+
+//ímpar ou par
+
+/*while (cont <= 15) {
+    if (cont % 2 == 0) {
+        console.log('Número par encontrado: ', cont)
+    } else {
+        console.log('Número ímpar encontrado: ', cont)
+    }
+    console.log('')
+    cont++
+}*/
+
+const palavra = 'calopsita'
+console.log('Ele tem uma ')
+
+do {
+    console.log(palavra[cont])
+    cont++
+} while (cont < palavra.length)
