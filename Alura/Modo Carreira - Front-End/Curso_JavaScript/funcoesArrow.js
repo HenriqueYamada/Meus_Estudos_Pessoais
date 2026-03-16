96463afa391a63582bb2e828ab4117876d88adfa
Henrique Yamada <henriqueyamada01@gmail.com>
2026-03-16 16:18:16 -0300

    updating arrow functions studies

--- Alura/Modo Carreira - Front-End/Curso_JavaScript/funcoesArrow.js
+++ Alura/Modo Carreira - Front-End/Curso_JavaScript/funcoesArrow.js
@@ -1,9 +1,11 @@
-const soma = (num1, num2) => {
+/*const soma = (num1, num2) => {
     return num1 + num2
 }
 
-console.log(soma(1, 2))
+console.log(soma(1, 2))*/
 
 //ou mais simplificado:
 
-const soma2 = (num1, num2) => console.log()
+const calculoDeSoma = (num1, num2) => num1 + num2 
+const soma = calculoDeSoma(2, 5)
+console.log(soma)
