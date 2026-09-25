c8db7386d18c90096015e8926f1bee8160579be3
Henrique Yuji Yamada <henriqueyamada01@gmail.com>
2026-09-25 08:26:35 -0300

    Feat: Reorganize JavaScript fundamentals files and restore script functionality

---
+++ JavaScript/Udemy/01-JavaScript_Fundamentals_Part_01/script.js
@@ -0,0 +1,18 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+let firstName = "Jonas";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
